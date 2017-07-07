d7c2e7071247c6286034844bf282a75a855ce147
jianye.tang <jianye.tang@aorise.org>
2017-07-07 12:22:33 +0800

    修改包名

---
+++ app/src/main/java/cn/aorise/sample/PlatformApplication.java
@@ -0,0 +1,39 @@
+package cn.aorise.sample;
+
+import android.util.Log;
+
+import cn.aorise.common.BaseApplication;
+import cn.aorise.sample.db.DbHelper;
+
+public class PlatformApplication extends BaseApplication {
+    private static final String TAG = PlatformApplication.class.getSimpleName();
+//    private RefWatcher mRefWatcher;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "onCreate");
+//        if (LeakCanary.isInAnalyzerProcess(this)) {
+//            // This process is dedicated to LeakCanary for heap analysis.
+//            // You should not init your app in this process.
+//            return;
+//        }
+//        mRefWatcher = LeakCanary.install(this);
+        DbHelper.getInstance().init(this);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+//    public static RefWatcher getRefWatcher(Context context) {
+//        PlatformApplication application = (PlatformApplication) context.getApplicationContext();
+//        return application.mRefWatcher;
+//    }
+}
